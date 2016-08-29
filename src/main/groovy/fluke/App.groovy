8465f2d09bf30a95c05fad0ae751a56d5badf789
Franz Garcia <franzpgarcia@gmail.com>
2016-08-28 22:33:26 -0400

    Fixed main method

--- src/main/groovy/fluke/App.groovy
+++ src/main/groovy/fluke/App.groovy
@@ -23,42 +23,7 @@ import fluke.script.ScriptRunner;
 class App implements BuiltInFunctions {
 
 	static void main(String[] args) {
-		def scriptStr = '''
-		def test() {
-			println "test1"
-		}
-		
-		procedure test: {
-			
-		}
-		
-		image hello: {
-			maintainer = "Franz Garcia"
-			onstart entrypoint: "/home/foo.sh", parameters: "-d"
-			from image: "ubuntu"
-			def f = {
-				println "test"
-			}
-			def a = "foo"
-			procedure test2: {
-				run shell, "mkdir", "/blah1"
-				with directory: "/home", {
-					run shell, "mkdir", "bla"
-				}
-				run shell, "mkdir", "/blah2"
-				port 22
-				copy file("C:\\\\Users\\\\Franz\\\\Desktop\\\\movies.txt"), "my_movies", "/home/"
-				//install package:"openssl"
-			}
-			apply procedure: "test2"
-			volume "~/bla"
-		}
-		
-		build image: "hello"
-		'''
 		FlukeCli flukeCli = new FlukeCli()
-		flukeCli.handle(["-A", "foo=hello", "-A", "hello=world", "C:\\Users\\Franz\\Desktop\\test.fluke"] as String[])
-		//flukeCli.handle(args)
-		
+		flukeCli.handle(args)
 	}
 }
