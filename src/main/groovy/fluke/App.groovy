85a1ca78f2d71e20a935272f425268dbe442829c
Franz-Philippe Garcia <franzpgarcia@gmail.com>
2016-09-01 13:28:37 -0400

    Fixed hardcoded path in App.groovy

--- src/main/groovy/fluke/App.groovy
+++ src/main/groovy/fluke/App.groovy
@@ -7,7 +7,6 @@ class App {
 
 	static void main(String[] args) {
 		FlukeCli flukeCli = new FlukeCli()
-		//flukeCli.handle(args)
-		flukeCli.handle(["C:\\Users\\Franz\\Desktop\\test.fluke"] as String[])
+		flukeCli.handle(args)
 	}
 }
